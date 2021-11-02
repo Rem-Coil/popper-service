e98690ec978d9dc430059f70b4d6b58e353aa3de
mishka361 <elotomp@gmail.com>
2021-11-02 13:49:19 +0300

    [POPPER-T-27] Поправил регистры, где надо было

--- src/main/kotlin/com/remcoil/data/model/action/Action.kt
+++ src/main/kotlin/com/remcoil/data/model/action/Action.kt
@@ -4,14 +4,19 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Action(
     val id: Int,
+    @SerialName("operator_id")
     val operatorId: Int,
+    @SerialName("bobbin_id")
     val bobbinId: Int,
+    @SerialName("action_type")
     val actionType: String,
+    @SerialName("done_time")
     val doneTime: LocalDateTime
 ) {
     constructor(identity: ActionIdentity): this(
--- src/main/kotlin/com/remcoil/data/model/bobbin/Bobbin.kt
+++ src/main/kotlin/com/remcoil/data/model/bobbin/Bobbin.kt
@@ -1,10 +1,13 @@
 package com.remcoil.data.model.bobbin
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 class Bobbin(
     val id: Int,
+    @SerialName("task_id")
     val taskId: Int,
+    @SerialName("bobbin_number")
     val bobbinNumber: String
 )
