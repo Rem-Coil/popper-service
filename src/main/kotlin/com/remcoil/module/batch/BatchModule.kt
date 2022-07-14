0a896446f0f275f4c7a9d3e96a7faa616ebb33b8
Ivanesca <elotomp@gmail.com>
2022-07-14 23:31:53 +0400

    [POPPER-T-119] - Коды по партиям, по несколько в строку

--- src/main/kotlin/com/remcoil/module/batch/BatchModule.kt
+++ src/main/kotlin/com/remcoil/module/batch/BatchModule.kt
@@ -2,16 +2,26 @@ package com.remcoil.module.batch
 
 import com.remcoil.data.model.batch.BatchIdentity
 import com.remcoil.service.batch.BatchService
+import com.remcoil.service.bobbin.BobbinService
 import com.remcoil.utils.safetyReceive
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.css.*
+import kotlinx.html.*
+import kotlinx.html.body
+import kotlinx.html.img
+import kotlinx.html.p
+import kotlinx.html.table
+import kotlinx.html.td
 import org.kodein.di.instance
 import org.kodein.di.ktor.closestDI
 
 fun Application.batchModule() {
     val batchService: BatchService by closestDI().instance()
+    val bobbinService: BobbinService by closestDI().instance()
 
     routing {
         route("/batch") {
@@ -52,6 +62,67 @@ fun Application.batchModule() {
                     call.respond(batch)
                 }
             }
+
+            get("/styles.css") {
+                call.respondCss {
+                    body {
+                        fontFamily = "Verdana"
+                    }
+                    table {
+                        margin(0.px, LinearDimension.auto)
+                    }
+                    td {
+                        textAlign = TextAlign.center
+                        padding(20.px, 40.px)
+                        fontSize = 12.pt
+                    }
+                    h3 {
+                        textAlign = TextAlign.center
+                    }
+                    p {
+                        padding(5.px)
+                    }
+                    tr {
+                        height = 150.px
+                    }
+                    img {
+                        height = 150.px
+                    }
+                }
+            }
+
+            get("/codes/{batch_id}") {
+                val bobbins = bobbinService.getByBatchId(call.parameters["batch_id"]!!.toLong())
+                val bobbinsIterator = bobbins.iterator()
+                call.respondHtml {
+                    head {
+                        link(rel = "stylesheet", href = "/batch/styles.css", type = "text/css")
+                    }
+                    body {
+                        table {
+                            while (bobbinsIterator.hasNext()) {
+                                tr {
+                                    for (i in 1..5) {
+                                        if (bobbinsIterator.hasNext()) {
+                                            val bobbin = bobbinsIterator.next()
+                                            td {
+                                                p {
+                                                    +bobbin.bobbinNumber
+                                                }
+                                                img(src = "https://api.qrserver.com/v1/create-qr-code/?data=bobbin:${bobbin.id}")
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CssBuilder.() -> Unit) {
+    this.respondText(CssBuilder().apply(builder).toString(), ContentType.Text.CSS)
 }
--- src/main/kotlin/com/remcoil/module/bobbin/BobbinModule.kt
+++ src/main/kotlin/com/remcoil/module/bobbin/BobbinModule.kt
@@ -2,22 +2,17 @@ package com.remcoil.module.bobbin
 
 import com.remcoil.data.model.bobbin.Bobbin
 import com.remcoil.service.bobbin.BobbinService
-import com.remcoil.service.task.TaskService
 import com.remcoil.utils.safetyReceive
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.kodein.di.instance
 
-import kotlinx.css.*
-import kotlinx.html.*
-import org.kodein.di.*
 import org.kodein.di.ktor.closestDI
 
 fun Application.bobbinModule() {
     val bobbinService: BobbinService by closestDI().instance()
-    val taskService: TaskService by closestDI().instance()
 
     routing {
         route("/bobbin") {
@@ -47,80 +42,7 @@ fun Application.bobbinModule() {
                 bobbinService.deleteById(call.parameters["id"]!!.toLong())
                 call.respond(HttpStatusCode.OK)
             }
-
-            get("/styles.css") {
-                call.respondCss {
-                    body {
-                        fontFamily = "Verdana"
-                    }
-                    table {
-                        margin(0.px, LinearDimension.auto)
-                    }
-                    td {
-                        margin(10.px)
-                        fontSize = 12.pt
-                    }
-                    h3 {
-                        textAlign = TextAlign.center
-                    }
-                    p {
-                        paddingLeft = 20.px
-                    }
-                    tr {
-                        height = 150.px
-                    }
-                    img {
-                        height = 100.px
-                        padding()
-                    }
-                }
-            }
-
-
-            get("/codes/{id}") {
-                val bobbins = bobbinService.getByBatchId(call.parameters["id"]!!.toLong())
-                val task = taskService.getById(call.parameters["id"]!!.toInt())
-
-                call.respondHtml {
-                    head {
-                        link(rel = "stylesheet", href = "/bobbin/styles.css", type = "text/css")
-                    }
-                    body {
-                        table {
-                            tr {
-                                td {
-                                    h3 {
-                                        +"ТЗ: ${task?.taskName} ${task?.taskNumber}"
-                                    }
-                                }
-                            }
-                            for (bobbin in bobbins) {
-                                tr {
-                                    td {
-                                        p {
-                                            +"Номер катушки: ${bobbin.bobbinNumber}"
-                                        }
-                                        p {
-                                            +"ID катушки: ${bobbin.id}"
-                                        }
-                                        p {
-                                            +"ID партии: ${bobbin.batchId}"
-                                        }
-                                    }
-                                    td {
-                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?data=bobbin:${bobbin.id}")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
         }
     }
 }
 
-suspend inline fun ApplicationCall.respondCss(builder: CssBuilder.() -> Unit) {
-    this.respondText(CssBuilder().apply(builder).toString(), ContentType.Text.CSS)
-}
