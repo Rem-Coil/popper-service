314747a7dda62dbf34f49fe886b1ca77bf4c396b
Ivanesca <elotomp@gmail.com>
2023-01-14 18:39:51 +0300

    [POPPER-T-163] - Одна страничка с разными вариантами

--- src/main/kotlin/com/remcoil/module/batch/BatchModule.kt
+++ src/main/kotlin/com/remcoil/module/batch/BatchModule.kt
@@ -141,50 +141,151 @@ fun Application.batchModule() {
                     }
                 }
             }
-            get("/{id}/codes/{codes_in_row}") {
-                val batchId = call.parameters["id"]!!.toLong()
-                val codesInRow = call.parameters["codes_in_row"]!!.toInt()
-                val batch = batchService.getById(batchId)
-                if (batch == null) {
-                    call.respondHtml { HttpStatusCode.NoContent }
-                    return@get
+
+            get("/example/styles.css") {
+                call.respondCss {
+                    body {
+                        fontFamily = "Verdana"
+                    }
+                    td {
+                        textAlign = TextAlign.center
+                        padding(10.px, 10.px)
+                        fontSize = 9.pt
+                    }
+                    h3 {
+                        textAlign = TextAlign.center
+                    }
+                    p {
+                        margin(0.px)
+                    }
+                    img {
+                        marginTop = 10.px
+                    }
                 }
-                val bobbins = bobbinService.getByBatchId(batchId)
-                val bobbinsIterator = bobbins.iterator()
+            }
+            get("/codes/example") {
                 call.respondHtml {
                     head {
-                        link(rel = "stylesheet", href = "/batch/styles.css", type = "text/css")
+                        link(rel = "stylesheet", href = "/batch/example/styles.css", type = "text/css")
                     }
                     body {
                         table {
                             tr {
-                                td {
-                                    p {
-                                        +"Партия"
-                                    }
-                                    p {
-                                        +batch.batchNumber
-                                    }
-                                    img(src = "https://api.qrserver.com/v1/create-qr-code/?data=batch:${batch.id}")
-                                }
-                            }
-                            while (bobbinsIterator.hasNext()) {
-                                tr {
-                                    for (i in 1..codesInRow) {
-                                        if (bobbinsIterator.hasNext()) {
-                                            val bobbin = bobbinsIterator.next()
-                                            td {
-                                                p {
-                                                    +"Катушка"
-                                                }
-                                                p {
-                                                    +bobbin.bobbinNumber
-                                                }
-                                                img(src = "https://api.qrserver.com/v1/create-qr-code/?data=bobbin:${bobbin.id}")
-                                            }
+                                for (i in 1..5) {
+                                    td {
+                                        p {
+                                            +"Катушка"
                                         }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${250}x${250}&data=bobbin:${i}")
                                     }
+
                                 }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..6) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${205}x${205}&data=bobbin:${i}")
+                                    }
+
+                                }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..7) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${173}x${173}&data=bobbin:${i}")
+                                    }
+
+                                }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..8) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${149}x${149}&data=bobbin:${i}")
+                                    }
+
+                                }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..9) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${(130)}x${130}&data=bobbin:${i}")
+                                    }
+
+                                }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..10) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${115}x${115}&data=bobbin:${i}")
+                                    }
+
+                                }
+
+                            }
+                        }
+                        table {
+                            tr {
+                                for (i in 1..11) {
+                                    td {
+                                        p {
+                                            +"Катушка"
+                                        }
+                                        p {
+                                            +"Номер катушки"
+                                        }
+                                        img(src = "https://api.qrserver.com/v1/create-qr-code/?size=${102}x${102}&data=bobbin:${i}")
+                                    }
+
+                                }
+
                             }
                         }
                     }
